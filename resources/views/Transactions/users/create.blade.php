cce2542c88712a38ebee712bcc679b0406c82cb5
deknaa <kdkrisnaaa@gmail.com>
2024-12-03 15:36:49 +0800

    chore(style): style input kelas & kendaraan

--- resources/views/Transactions/users/create.blade.php
+++ resources/views/Transactions/users/create.blade.php
@@ -16,9 +16,9 @@
                     </select>
                 </div>
                 
-                <div id="ruangKelasSelect" style="display: none;">
-                    <label for="ruang_kelas_id">Pilih Ruang Kelas</label>
-                    <select name="ruang_kelas_id" id="ruang_kelas_id">
+                <div id="ruangKelasSelect" style="display: none;" class="max-w-sm mt-3">
+                    <label for="ruang_kelas_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih Ruang Kelas</label>
+                    <select name="ruang_kelas_id" id="ruang_kelas_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="" disabled selected>Pilih Ruang Kelas</option>
                         @foreach ($ruangKelas as $ruang)
                             <option value="{{ $ruang->id }}">{{ $ruang->nama_ruangan }}</option>
@@ -26,9 +26,9 @@
                     </select>
                 </div>
             
-                <div id="kendaraanSelect" style="display: none;">
-                    <label for="kendaraan_id">Pilih Kendaraan</label>
-                    <select name="kendaraan_id" id="kendaraan_id">
+                <div id="kendaraanSelect" style="display: none;" class="max-w-sm mt-3">
+                    <label for="kendaraan_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pilih Kendaraan</label>
+                    <select name="kendaraan_id" id="kendaraan_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                         <option value="" disabled selected>Pilih Kendaraan</option>
                         @foreach ($kendaraan as $kend)
                             <option value="{{ $kend->id }}">{{ $kend->nama_kendaraan }}</option>
